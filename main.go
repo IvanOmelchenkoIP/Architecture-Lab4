0eb70e24179b528e98cf302f15b4b4acc50909f4
IvanOmelchenkoIP <io25112002@gmail.com>
2022-06-29 11:19:05 +0300

    Add commands implementation
    Add implementation of commands print and reverse

--- main.go
+++ main.go
@@ -1,6 +1,8 @@
 package main
 
-import ( "fmt" )
+import (
+	"fmt"
+)
 
 type Command interface {
 	Execute(handler Handler)
@@ -8,6 +10,28 @@ type Command interface {
 
 type Handler interface {
 	Post(cmd Command)
+}
+
+type CommandPrint struct {
+	str string
+}
+
+func (print *CommandPrint) Execute(handler Handler) {
+	fmt.Println(print.str)
+}
+
+type CommandReverse struct {
+	str string
+}
+
+func (reverse *CommandReverse) Execute(handler Handler) {
+	strBytes := []rune(reverse.str)
+	reversedBytes := make([]rune, 0)
+	for i := len(strBytes) - 1; i >= 0; i-- {
+		reversedBytes = append(reversedBytes, strBytes[i])
+	}
+	reversedStr := string(reversedBytes)
+	handler.Post(&CommandPrint{str: reversedStr})
 }
 
 func main() {
